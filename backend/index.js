7430596a3622e408b8395f5a976f6280cf0b65b7
Techolution <techolution@Lakshya-Makode.local>
2025-12-12 01:19:16 +0530

    Added new endpoint in the backend to get the user by it's name

--- backend/index.js
+++ backend/index.js
@@ -72,6 +72,20 @@ app.post("/deleteUser", async (req, res)=>{
         console.log("Error in deleting the User", error);
         res.status(500).send({message:"Error in deleting the user"});
     }
+});
+
+app.post("/getUserByName", async (req, res)=>{
+    try {
+        const {userName} = req.body;
+        const userDetails = await userModel.findOne({userName:userName});
+        if(!userDetails){
+            return res.status(404).send({message:"User details not found by its name"});
+        }
+        res.status(200).json({userDetails:userDetails});
+    } catch (error) {
+        console.log("Error in getting the user by its name", error);
+        res.status(500).send({message:"Error in getting the user by its name"});
+    }
 })
 
 const PORT = 5000;
